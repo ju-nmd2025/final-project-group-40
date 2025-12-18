94d8bd2e380c7eb9643711fa77127d2dcdcaca23
Sophia <olso24gz@student.ju.se>
2025-12-18 15:16:01 +0100

    set width identical for all platforms

--- platform.js
+++ platform.js
@@ -17,96 +17,86 @@ class Platform {
     return this.y > height + 50;
   }
 
-
-show() {
-  push();
-  fill(150);
-  noStroke();
-  rect(this.x, this.y, this.w, this.h, 5);
-  pop();
-}
+  show() {
+    push();
+    fill(150);
+    noStroke();
+    rect(this.x, this.y, this.w, this.h, 5);
+    pop();
+  }
 }
 
 //======BAMBMOO PLATFORM=====//
 
 class BambooPlatform extends Platform {
-  constructor(x, y, w = 240, h = 20) {
+  constructor(x, y, w, h = 20) {
     super(x, y, w, h);
   }
 
-show() {
-  push();
-  translate(this.x, this.y);
+  show() {
+    push();
+    translate(this.x, this.y);
 
-  // Main bamboo platform
+    // Main bamboo platform
     noStroke();
     fill(120, 180, 90); // bamboo green
     rect(0, 0, this.w, this.h, 10);
 
-    // Bamboo 
+    // Bamboo
     stroke(90, 140, 70);
     strokeWeight(3);
     for (let i = 30; i < this.w; i += 40) {
-        line (i, 0, i, this.h);
+      line(i, 0, i, this.h);
     }
 
     // Highlights
     noStroke();
-    fill(150, 210, 120 , 150);
+    fill(150, 210, 120, 150);
 
     rect(4, 4, this.w - 8, this.h / 3, 8);
-
 
     pop();
   }
 }
-  
-  //======SUSHI PLATFORM====//
 
-    class SushiPlatform extends Platform {
-      constructor (x, y, w = 260, h = 70) {
-        super(x, y, w, h);
-      }
+//======SUSHI PLATFORM====//
 
-      show() {
-        push();
-        translate(this.x, this.y);
-        rectMode (CENTER);
-        noStroke();
+class SushiPlatform extends Platform {
+  constructor(x, y, w, h = 70) {
+    super(x, y, w, h);
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    rectMode(CENTER);
+    noStroke();
 
     // Rice shape
     fill(220); // white rice
     ellipse(0, 32, this.w * 0.95, this.h * 0.45);
 
-    // Rice base 
+    // Rice base
     fill(250); // white rice
     stroke(230);
     strokeWeight(2);
     ellipse(0, 22, this.w, this.h * 0.6);
 
-    // Rice details 
-    fill (230);
+    // Rice details
+    fill(230);
     for (let i = -this.w / 2 + 15; i < this.w / 2; i += 20) {
-        for (let j = 12; j < 32; j += 8) {
-            ellipse(i + random(-1, 2), j + random(-1, 1), 3 , 3);
-        }
+      for (let j = 12; j < 32; j += 8) {
+        ellipse(i + random(-1, 2), j + random(-1, 1), 3, 3);
+      }
     }
 
-    // Salmon part 
+    // Salmon part
     fill(255, 120, 120); // pink salmon
     beginShape();
     vertex(-this.w / 2 + 20, 10);
-    bezierVertex (
-        -this.w / 4, -35, 
-        this.w / 4, -35,
-        this.w / 2 -20, 10
-    );
+    bezierVertex(-this.w / 4, -35, this.w / 4, -35, this.w / 2 - 20, 10);
     vertex(this.w / 2 - 10, 30);
-    bezierVertex(
-        this.w / 4, 10,
-        -this.w / 4, 10,
-        -this.w / 2 + 10, 30
-    );
+    bezierVertex(this.w / 4, 10, -this.w / 4, 10, -this.w / 2 + 10, 30);
     endShape(CLOSE);
 
     // Salmo stripes
@@ -114,28 +104,22 @@ show() {
     strokeWeight(3);
     noFill();
     for (let i = -this.w / 2 + 40; i < this.w / 4; i += 35) {
-        bezier(
-            i, 18,
-            i + 10, 5,
-            i + 20, 5, 
-            i + 30, 10
-        );
+      bezier(i, 18, i + 10, 5, i + 20, 5, i + 30, 10);
     }
 
     // Highlights
     noStroke();
-    fill(255,200);
+    fill(255, 200);
     ellipse(0, -5, this.w * 0.6, 18);
-    
-pop();
-}
- }
 
+    pop();
+  }
+}
 
 //======CLOUD PLATFORM====//
 
 class CloudPlatform extends Platform {
-  constructor(x, y, w=140, h=20) {
+  constructor(x, y, w, h = 20) {
     super(x, y, w, h);
     this.color = random() > 0.5 ? color(280, 185, 205) : color(180, 190, 200);
 
